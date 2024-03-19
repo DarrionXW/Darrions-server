7ffd7608b0dd75006c9825d0e004b9cb4c3a8390
DarrionXW <164020605+DarrionXW@users.noreply.github.com>
2024-03-19 15:03:29 +0000

    server/world/ 	server/world_nether/ 	server/world_the_end/

--- web/eaglercraft_opts.js
+++ web/eaglercraft_opts.js
@@ -3,8 +3,8 @@ window.eaglercraftXOpts = () => ({
   // joinServer: "ws" + window.location.href.slice(4),
   // assetsURI: "custom.epk",
 	servers: [
-		{ addr: "ws" + window.location.href.slice(4), name: "EaglercraftX Server" }
+		{ addr: "ws" + window.location.href.slice(4), name: "Darrion's server" }
 	]
 });
-// document.title = "My Original Server Name:tm:";
+// document.title = "My Original Server:tm:";
 // document.querySelector("link[rel=\"shortcut icon\"]").href = "https://www.google.com/favicon.ico";
